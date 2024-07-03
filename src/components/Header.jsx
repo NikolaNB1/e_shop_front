7ee6ca2db38f31483d4ad27398effac15b433487
Nikola Babic <babicn19@gmail.com>
2024-07-03 13:23:58 +0200

    layout and api

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="nav affix">
+        <div className="container">
+          <div className="logo">
+            <Link to="/">E shop</Link>
+          </div>
+          <div id="mainListDiv" className="main_list">
+            <ul className="navlinks">
+              <li>
+                <Link to="#">Products</Link>
+              </li>
+              <li>
+                <Link to="#"></Link>
+              </li>
+              <li>
+                <Link to="#">Login</Link>
+              </li>
+              <li>
+                <Link to="#">Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
